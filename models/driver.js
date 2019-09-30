1a846bce417876b269c5fde2ecc7d88f6e36deec
Jukka Holopainen <jukka.holopainen3@metropolia.fi>
2019-09-30 10:25:37 +0300

    Driver model tehty varmaan ja vähän vehiclestä

--- models/driver.js
+++ models/driver.js
@@ -1,2 +1,47 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+
+const driverSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true
+    },
+    nationality: {
+        type: String,
+    },
+    organization: {
+        type: String,
+    },
+    session: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Session'
+    }],
+    passwordHash: {
+        type: String,
+        required: true
+
+    },
+    userName: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    vehicle: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Vehicle'
+    }]
+})
+
+driverSchema.plugin(uniqueValidator)
+
+driverSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+const Driver = mongoose.model('Driver', driverSchema)
+
+module.exports = Driver
--- models/vehicle.js
+++ models/vehicle.js
@@ -1,2 +1,32 @@
 const mongoose = require('mongoose')
 const uniqueValidator = require('mongoose-unique-validator')
+
+const vehicleSchema = new mongoose.Schema({
+    driver: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Driver'
+    },
+    class: {
+        type: String,
+    },
+    name: {
+        type: String,
+    },
+    description: {
+        type: String,
+    }
+})
+
+driverSchema.plugin(uniqueValidator)
+
+driverSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+        delete returnedObject.passwordHash
+    }
+})
+const Driver = mongoose.model('Driver', driverSchema)
+
+module.exports = Driver
